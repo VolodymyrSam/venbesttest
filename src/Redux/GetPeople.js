d6a3ba8565a729e14520f25afabd419624bc6092
Vladimir <vova290594@gmail.com>
2019-12-24 16:44:18 +0200

    Edit numbering

--- src/Redux/GetPeople.js
+++ src/Redux/GetPeople.js
@@ -2,8 +2,14 @@ import {rerenderDOM} from './../index.js';
 import {RefOfArea} from './../index.js';
 import rerenderPeople from './../components/MyHeader/rerenderPeople.js';
 import sorting from './../components/MyHeader/sortingPeople.js';
+import Axios from 'axios';
+//import {GetDada} from '';
 
 let GetvenbastUsers = (store, flag) => {
+  // let res2 = Axios.get('https://venbest-test.herokuapp.com/').then(res => {
+  //   console.log(res);
+  // })
+  // console.log(res2);
   if(flag == 1) {
   window.fetch("https://venbest-test.herokuapp.com/")
     .then(res => res.json())
