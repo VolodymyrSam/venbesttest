f4088a92319b66f02911811906199cb635acf334
Vladimir <vova290594@gmail.com>
2019-12-29 02:43:27 +0200

    Updates

---
+++ precache-manifest.c2fca323779bbafc6c185192d2501254.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb25062eef001592646693d614ed9a66",
+    "url": "/venbesttest/index.html"
+  },
+  {
+    "revision": "859caf75f79d818ee260",
+    "url": "/venbesttest/static/css/main.8736f041.chunk.css"
+  },
+  {
+    "revision": "602d1c7820ca1b97ebd3",
+    "url": "/venbesttest/static/js/2.6a37ad08.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/venbesttest/static/js/2.6a37ad08.chunk.js.LICENSE"
+  },
+  {
+    "revision": "859caf75f79d818ee260",
+    "url": "/venbesttest/static/js/main.61e5a090.chunk.js"
+  },
+  {
+    "revision": "0bbae24aa8ea7fcc0c76",
+    "url": "/venbesttest/static/js/runtime-main.0a72cfaa.js"
+  },
+  {
+    "revision": "dec96521aee87b0d1326e82377243dcb",
+    "url": "/venbesttest/static/media/pattern.dec96521.png"
+  }
+]);
